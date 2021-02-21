0488d03ebe3a34e49662709825945b2e361539b7
hackermdch <594524502@qq.com>
2021-02-21 21:11:56 +0800

    update

--- src/main/java/com/hacker/programblock/ProgramBlockScreen.java
+++ src/main/java/com/hacker/programblock/ProgramBlockScreen.java
@@ -20,7 +20,13 @@ public class ProgramBlockScreen extends Screen {
         codes = new TextAreaWidget(this.font, this.width / 2 - 150, 50, 300, 100, new TranslationTextComponent("program.code"));
         codes.setMaxStringLength(Integer.MAX_VALUE);
         children.add(codes);
+        setFocusedDefault(codes);
         codes.setFocused2(true);
+    }
+
+    @Override
+    public void tick() {
+        codes.tick();
     }
 
     @Override
@@ -37,10 +43,10 @@ public class ProgramBlockScreen extends Screen {
             minecraft.displayGuiScreen(null);
             return true;
         }
-        if (keyCode == GLFW.GLFW_KEY_ENTER) {
-            codes.setText(codes.getText() + "\n");
-            return true;
-        }
+//        if (keyCode == GLFW.GLFW_KEY_ENTER) {
+//            codes.setText(codes.getText() + "\n");
+//            return true;
+//        }
         return super.keyPressed(keyCode, scanCode, modifiers);
     }
 }
--- src/main/java/com/hacker/programblock/TextAreaWidget.java
+++ src/main/java/com/hacker/programblock/TextAreaWidget.java
@@ -2,17 +2,23 @@ package com.hacker.programblock;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.AbstractGui;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.util.SharedConstants;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.text.ITextComponent;
 
 import javax.annotation.Nullable;
 
+import static org.lwjgl.glfw.GLFW.*;
+
+@SuppressWarnings({"NullableProblems", "unused"})
 public class TextAreaWidget extends TextFieldWidget {
     private int col = 0;
-    private int row = 0;
+    private int row = 1;
+    private int scroll = 0;
 
     public TextAreaWidget(FontRenderer p_i232260_1_, int p_i232260_2_, int p_i232260_3_, int p_i232260_4_, int p_i232260_5_, ITextComponent p_i232260_6_) {
         super(p_i232260_1_, p_i232260_2_, p_i232260_3_, p_i232260_4_, p_i232260_5_, p_i232260_6_);
@@ -46,58 +52,198 @@ public class TextAreaWidget extends TextFieldWidget {
                 if (this.isEnabled) {
                     this.writeText("");
                 }
-
                 return true;
             } else {
                 switch (keyCode) {
-                    case 259:
+                    case GLFW_KEY_BACKSPACE:
                         if (this.isEnabled) {
                             this.field_212956_h = false;
                             this.delete(-1);
                             this.field_212956_h = Screen.hasShiftDown();
                         }
-
                         return true;
-                    case 260:
-                    case 264:
-                    case 265:
-                    case 266:
+                    case GLFW_KEY_INSERT:
+                    case GLFW_KEY_PAGE_UP:
+                    case GLFW_KEY_PAGE_DOWN:
                     default:
                         return false;
-                    case 267:
+                    case GLFW_KEY_ENTER:
+                        addText("\n");
+                        row++;
+                        col = 0;
                         return true;
-                    case 261:
+                    case GLFW_KEY_DELETE:
                         if (this.isEnabled) {
                             this.field_212956_h = false;
                             this.delete(1);
                             this.field_212956_h = Screen.hasShiftDown();
                         }
                         return true;
-                    case 262:
-                        if (Screen.hasControlDown()) {
-                            this.setCursorPosition(this.getNthWordFromCursor(1));
-                        } else {
-                            this.moveCursorBy(1);
+                    case GLFW_KEY_RIGHT:
+                        String[] ss = getLines();
+                        col++;
+                        int tmpr = row;
+                        if (col > ss[row - 1].length()) {
+                            row++;
                         }
-
+                        row = Math.min(row, ss.length);
+                        if (tmpr != row)
+                            col = 0;
+                        col = MathHelper.clamp(col, 0, ss[row - 1].length());
                         return true;
-                    case 263:
-                        if (Screen.hasControlDown()) {
-                            this.setCursorPosition(this.getNthWordFromCursor(-1));
-                        } else {
-                            this.moveCursorBy(-1);
+                    case GLFW_KEY_LEFT:
+                        ss = getLines();
+                        col--;
+                        tmpr = row;
+                        if (col < 0) {
+                            row--;
                         }
-
+                        row = Math.max(row, 1);
+                        if (tmpr != row)
+                            col = ss[row - 1].length();
+                        col = MathHelper.clamp(col, 0, ss[row - 1].length());
                         return true;
-                    case 268:
+                    case GLFW_KEY_UP:
+                        String[] st = getLines();
+                        row--;
+                        row = Math.max(row, 1);
+                        if (col > st[row - 1].length()) {
+                            col = st[row - 1].length();
+                        }
+                        return true;
+                    case GLFW_KEY_DOWN:
+                        String[] sa = getLines();
+                        row++;
+                        row = Math.min(row, sa.length);
+                        if (col > sa[row - 1].length()) {
+                            col = sa[row - 1].length();
+                        }
+                        return true;
+                    case GLFW_KEY_HOME:
                         this.setCursorPositionZero();
                         return true;
-                    case 269:
+                    case GLFW_KEY_END:
                         this.setCursorPositionEnd();
                         return true;
                 }
             }
         }
+    }
+
+    @Override
+    public void setCursorPositionZero() {
+        row = 1;
+        col = 0;
+    }
+
+    private String[] getLines() {
+        String[] ss = text.split("\n");
+        if (text.endsWith("\n")) {
+            String[] sss = new String[ss.length + 1];
+            System.arraycopy(ss, 0, sss, 0, ss.length);
+            sss[ss.length] = "";
+            ss = sss;
+        }
+        return ss;
+    }
+
+    @Override
+    protected void delete(int num) {
+        deleteFromCursor(num);
+    }
+
+    @Override
+    public void deleteWords(int num) {
+        deleteFromCursor(num);
+    }
+
+    @Override
+    public void deleteFromCursor(int num) {
+        if (!this.text.isEmpty()) {
+            String[] ss = getLines();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < ss.length; i++) {
+                if (i == row - 1) {
+                    if (num < 0) {
+                        if (col == 0 && row != 1) {
+                            row--;
+                            col = ss[i - 1].length();
+                            ss[i - 1] += ss[i];
+                            ss[i] = "@$%^[{skip}]@$%^;";
+                            break;
+                        }
+                        try {
+                            sb.append(ss[i], 0, col + num);
+                            sb.append(ss[i].substring(Math.min(col, ss[i].length())));
+                        } catch (Exception ignored) {
+                            break;
+                        }
+                        ss[i] = sb.toString();
+                        col = MathHelper.clamp(col - 1, 0, ss[i].length());
+                        break;
+                    } else if (num > 0) {
+                        try {
+                            sb.append(ss[i], 0, col - num + 1);
+                            sb.append(ss[i].substring(Math.min(col + 1, ss[i].length())));
+                            if (col > ss[i].length() - 1) {
+                                if (i + 1 < ss.length)
+                                    if (row + 1 <= ss.length) {
+                                        sb.append(ss[i + 1]);
+                                        ss[i + 1] = "@$%^[{skip}]@$%^;";
+                                    }
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        ss[i] = sb.toString();
+                        col = MathHelper.clamp(col, 0, ss[i].length());
+                        break;
+                    }
+                }
+            }
+            sb = new StringBuilder();
+            for (int i = 0; i < ss.length; i++) {
+                if (ss[i].equals("@$%^[{skip}]@$%^;"))
+                    continue;
+                sb.append(ss[i]);
+                if (i != ss.length - 1)
+                    sb.append("\n");
+            }
+            this.text = sb.toString();
+        }
+    }
+
+    @Override
+    public void setText(String textIn) {
+        if (this.validator.test(textIn)) {
+            if (textIn.length() > this.maxStringLength) {
+                this.text = textIn.substring(0, this.maxStringLength);
+            } else {
+                this.text = textIn;
+            }
+
+            this.setSelectionPos(this.cursorPosition);
+            this.onTextChanged(textIn);
+        }
+    }
+
+    @Override
+    public void setCursorPositionEnd() {
+        String[] ss = getLines();
+        row = ss.length;
+        col = ss[ss.length - 1].length();
+    }
+
+    public void setCursorRow(int row) {
+        this.row = MathHelper.clamp(row, 1, getLines().length);
+    }
+
+    public void setCursorCol(int col) {
+        this.col = col;
+        if (!this.field_212956_h) {
+            this.setCursorCol(this.col);
+        }
+        this.onTextChanged(this.text);
     }
 
     @Override
@@ -108,7 +254,6 @@ public class TextAreaWidget extends TextFieldWidget {
             if (this.isEnabled) {
                 this.writeText(Character.toString(codePoint));
             }
-
             return true;
         } else {
             return false;
@@ -116,7 +261,97 @@ public class TextAreaWidget extends TextFieldWidget {
     }
 
     @Override
-    public void renderButton(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+    public void writeText(String textToWrite) {
+        if (this.validator.test(textToWrite)) {
+            addText(textToWrite);
+            col += textToWrite.length();
+            onTextChanged(this.text);
+        }
+    }
 
+    private void addText(String text) {
+        String[] ss = getLines();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < ss.length; i++) {
+            if (i == row - 1) {
+                sb.append(ss[i], 0, col);
+                sb.append(text);
+                sb.append(ss[i], col, ss[i].length());
+                ss[i] = sb.toString();
+                break;
+            }
+        }
+        sb = new StringBuilder();
+        for (int i = 0; i < ss.length; i++) {
+            sb.append(ss[i]);
+            if (i != ss.length - 1)
+                sb.append("\n");
+        }
+        this.text = sb.toString();
+    }
+
+    @Override
+    public boolean mouseScrolled(double mouseX, double mouseY, double delta) {
+        scroll += (int) -delta;
+        scroll = MathHelper.clamp(scroll, 0, getLines().length);
+        return true;
+    }
+
+    @Override
+    public void renderButton(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+        if (visible) {
+            if (enableBackgroundDrawing) {
+                int i = this.isFocused() ? -1 : -6250336;
+                fill(matrixStack, this.x - 1, this.y - 1, this.x + this.width + 1, this.y + this.height + 1, i);
+                fill(matrixStack, this.x, this.y, this.x + this.width, this.y + this.height, -16777216);
+            }
+            int color = this.isEnabled ? this.enabledColor : this.disabledColor;
+            String[] ss = getLines();
+            boolean showCursor = this.isFocused() && cursorCounter / 6 % 2 == 0;
+            boolean cursorInText = false;
+            int curX = 0;
+            int curY = 0;
+            for (int i = scroll; i < ss.length; i++) {
+                int x = this.enableBackgroundDrawing ? this.x + 4 : this.x;
+                int y = this.enableBackgroundDrawing ? this.y + (i - scroll) * 8 : this.y;
+                int x1 = x;
+                String s = this.fontRenderer.func_238412_a_(ss[i].substring(lineScrollOffset), this.getAdjustedWidth());
+                boolean flag = ss[i].length() > s.length();
+                if (i == row - 1) {
+                    if (flag)
+                        s = this.fontRenderer.func_238412_a_(ss[i].substring(Math.max(0, col - s.length())), this.getAdjustedWidth());
+                    if (!s.isEmpty()) {
+                        if (flag)
+                            x1 = this.fontRenderer.func_238407_a_(matrixStack, this.textFormatter.apply(s, lineScrollOffset), x, y, color);
+                        else {
+                            x1 = this.fontRenderer.func_238407_a_(matrixStack, this.textFormatter.apply(ss[i].substring(0, col), lineScrollOffset), x, y, color);
+                        }
+                    }
+                    curX = x1;
+                    curY = y;
+                    cursorInText = col < s.length();
+                    if (cursorInText) {
+                        curX = x1 - 1;
+                        --x1;
+                    }
+                    if (!s.isEmpty() && !flag) {
+                        this.fontRenderer.func_238407_a_(matrixStack, this.textFormatter.apply(ss[i].substring(col), col), x1, y, color);
+                    }
+                } else {
+                    if (flag)
+                        s = this.fontRenderer.func_238412_a_(col - ss[row - 1].length() < ss[i].length() && col - ss[row - 1].length() >= 0 ? ss[i].substring(col - ss[row - 1].length()) : "", this.getAdjustedWidth());
+                    if (!s.isEmpty()) {
+                        this.fontRenderer.func_238407_a_(matrixStack, this.textFormatter.apply(s, lineScrollOffset), x, y, color);
+                    }
+                }
+            }
+            if (showCursor) {
+                if (cursorInText) {
+                    AbstractGui.fill(matrixStack, curX, curY - 1, curX + 1, curY + 1 + 9, -3092272);
+                } else {
+                    this.fontRenderer.drawStringWithShadow(matrixStack, "_", curX, curY, color);
+                }
+            }
+        }
     }
 }

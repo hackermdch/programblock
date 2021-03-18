f4a2de1886f6a0ead815673856d3a3988a170d3b
hackermdch <58121864+hackermdch@users.noreply.github.com>
2021-03-19 00:09:52 +0800

    update

--- src/main/java/com/hacker/programblock/Rending.java
+++ src/main/java/com/hacker/programblock/Rending.java
@@ -6,21 +6,17 @@ import static org.lwjgl.opengl.GL20.*;
 
 class Rending {
     private static int program;
-    private static final String vsc = "#version 110" + '\n' +
-            "attribute vec3 pos;" + '\n' +
-            "attribute vec3 color;" + '\n' +
-            "varying vec3 Color;" + '\n' +
-            "void main()" + '\n' +
-            "{" + '\n' +
-            "   gl_Position = vec4(pos,1.0);" + '\n' +
-            "   Color = color;" + '\n' +
-            "}";
-
-    private static final String psc = "#version 110" + '\n' +
-            "varying vec3 Color;\n" +
+    private static final String vsc = "#version 110\n" +
+            "attribute vec3 aPos;\n" +
             "void main()\n" +
             "{\n" +
-            "    gl_FragColor = vec4(Color,1.0);\n" +
+            "   gl_Position = vec4(aPos, 1.0);\n" +
+            "}";
+
+    private static final String psc = " #version 110\n" +
+            "void main()\n" +
+            "{\n" +
+            "   gl_FragColor = vec4(1.0, 0.5, 0.2, 1.0);\n" +
             "}";
 
     static {
@@ -55,32 +51,28 @@ class Rending {
     }
 
     public static void draw() {
-        glPushAttrib(GL_ALL_ATTRIB_BITS);
-        glPushMatrix();
-        float[] vertices = {
-                0f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // 右上
-                0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,   // 右下
-                -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,   // 左下
-                -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f,    // 左上
+        float[] vets = new float[]{
+                0.5f, 0.5f, 0f,
+                0.5f, -0.5f, 0f,
+                -0.5f, -0.5f, 0f,
+//                -0.5f, 0.5f, 0
         };
-        int VBO = glGenBuffers();
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW);
         int[] indices = {
-                0, 1, 3,
-                1, 2, 3
+                0, 1, 2,
+                2, 3, 0
         };
-        int EBO = glGenBuffers();
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        int vbo = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, vets, GL_STATIC_DRAW);
+        int ebo = glGenBuffers();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW);
-        glVertexAttribPointer(0, 3, GL_FLOAT, false, 8 * Float.BYTES, 0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * Float.BYTES, 0);
         glEnableVertexAttribArray(0);
-        glVertexAttribPointer(1, 3, GL_FLOAT, false, 8 * Float.BYTES, 3 * Float.BYTES);
-        glEnableVertexAttribArray(1);
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
         glUseProgram(program);
-        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
-        glPopMatrix();
-        glPopAttrib();
+//        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
     }
 }

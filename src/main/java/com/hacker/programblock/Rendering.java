6f987dca0e22b82773554437b4db2273668aa61d
hackermdch <58121864+hackermdch@users.noreply.github.com>
2021-03-19 20:51:03 +0800

    update

--- src/main/java/com/hacker/programblock/Rendering.java
+++ src/main/java/com/hacker/programblock/Rendering.java
@@ -55,21 +55,48 @@ class Rendering {
         glDeleteShader(ps);
     }
 
-    public static void draw() {
-        float[] vertices = {
-                0.0f, 0.5f, 0.0f, 1, 0, 0, 1,
-                -0.5f, -0.5f, 0.0f, 0, 1, 0, 1,
-                0.5f, -0.5f, 0.0f, 0, 0, 1, 1
-        };
+    private static void begin() {
         glPushAttrib(GL_ALL_ATTRIB_BITS);
         glPushMatrix();
+        glEnable(GL_MULTISAMPLE);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
         glUseProgram(program);
+    }
+
+    private static void EnableAA() {
+        glEnable(GL_POINT_SMOOTH);
+        glEnable(GL_LINE_SMOOTH);
+        glEnable(GL_POLYGON_SMOOTH);
+    }
+
+    private static void end() {
+        glBindVertexArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glUseProgram(0);
+        glPopMatrix();
+        glPopAttrib();
+    }
+
+    public static void draw() {
+        float[] vertices = {
+                0.5f, 0.5f, 0.0f, 1, 0, 0, 1f,
+                0.5f, -0.5f, 0.0f, 0, 1, 0, 1f,
+                -0.5f, -0.5f, 0.0f, 0, 0, 1, 1f,
+                -0.5f, 0.5f, 0f, 0, 1, 1, 1,
+        };
+        begin();
         int vbo = glGenBuffers();
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
         glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW);
 
         int vao = glGenVertexArrays();
         glBindVertexArray(vao);
+        int[] inds = {0, 2, 1, 3, 2, 0};
+        int ebo = glGenBuffers();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, inds, GL_STATIC_DRAW);
+
         glEnableVertexAttribArray(0);
         glEnableVertexAttribArray(1);
 
@@ -78,11 +105,7 @@ class Rendering {
         glVertexAttribPointer(1, 4, GL_FLOAT, false, 28, 12);
 
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glBindVertexArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-        glUseProgram(0);
-        glPopMatrix();
-        glPopAttrib();
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        end();
     }
 }
